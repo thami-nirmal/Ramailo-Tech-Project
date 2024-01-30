ce8e0bba561b71c156714296883175ea4cc944da
nirmal <thaminirmal112@gmail.com>
2024-01-30 23:47:45 +0545

    registered the models in admin site

--- src/library/admin.py
+++ src/library/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import User, Book, BookDetails, BorrowedBooks
 
-# Register your models here.
+# Register the models with Django admin site
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    # Customize the display of User model in the admin site
+    list_display = ['user_id','name','email','membership_date']
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # Customize the display of Book model in the admin site
+    list_display = ['book_id','title','isbn','published_date','genre']
+
+
+@admin.register(BookDetails)
+class BookDetailsAdmin(admin.ModelAdmin):
+    # Cutomize the display of BookDetails model in the admin site
+    list_display = ['details_id','book_id','number_of_pages','publisher','language']
+
+
+@admin.register(BorrowedBooks)
+class BorrowedBooksrAdmin(admin.ModelAdmin):
+    # Cutomize the display of BorrowedBooks model in the admin site
+    list_display = ['user_id','book_id','borrow_date','return_date']
+
+
--- src/library/models.py
+++ src/library/models.py
@@ -3,15 +3,17 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
+    # Define fields for User model
     user_id              = models.AutoField(primary_key=True)
     name                 = models.CharField(max_length=100)
     email                = models.EmailField(max_length=254, unique=True)
-    membership_date      = models.DateField(auto_now_add=True)
+    membership_date      = models.DateField()
 
     def __str__(self):
         return self.name
     
 class Book(models.Model):
+    # Define fields for Book model
     book_id              = models.AutoField(primary_key=True)
     title                = models.CharField(max_length=100)
     isbn                 = models.CharField(max_length=13, unique=True)
@@ -22,6 +24,7 @@ class Book(models.Model):
         return self.title
     
 class BookDetails(models.Model):
+    # Define fields for BookDetails model
     details_id          = models.AutoField(primary_key=True)
     book_id             = models.OneToOneField(Book, related_name='bookDetails_book',on_delete=models.CASCADE)
     number_of_pages     = models.IntegerField()
@@ -32,6 +35,7 @@ class BookDetails(models.Model):
         return str(self.details_id)
     
 class BorrowedBooks(models.Model):
+    # Define fields for BorrowedBooks model
     user_id             = models.ForeignKey(User, related_name='borrowedBooks_user',on_delete=models.CASCADE)
     book_id             = models.ForeignKey(Book, related_name='borrowedBooks_book',on_delete=models.CASCADE)
     borrow_date         = models.DateField()
